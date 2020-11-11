ab195ea042b747739957cfe25503fcc270d4d838
LuisYela <javyela@gmail.com>
2020-11-11 02:41:33 -0600

    configuraciones de ip para que permita el acceso

--- backEnd/controllers/data.js
+++ backEnd/controllers/data.js
@@ -10,11 +10,11 @@ client.on('connect', function(){
 
 dataCtrl.getData = async (req,res) => {
     
-    /*/res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://frontend-srv:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-    */
+    //*/
     const datos = await data.find();
     let datosFormR = client.hgetall('datosCovid',(err,obj)=>{
         if(!obj){
--- backEnd/index.js
+++ backEnd/index.js
@@ -9,10 +9,10 @@ app.set('port', process.env.PORT || 3000);
 //midelwares
 
 app.use(cors({origin: 'http://frontend-srv:4200'}));
-/*/app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', 'http://frontend-srv:4200');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
